ea5b97abd26b055da1ffdc1d854331a161f9b6ab
Rishab Goyal <rishab954@gmail.com>
2016-03-11 01:01:21 +0530

    Updated

--- Data-Structures/LinkedList/Dobly-LinkedList.cpp
+++ Data-Structures/LinkedList/Dobly-LinkedList.cpp
@@ -6,14 +6,49 @@ struct node{
   node* next;
   node* prev;
 };
-node* insert(node* head, int data)
+
+void insert(node* head, int data)
 {
   node* temp = new node();
-  temp->next = NULL;
+  temp->next = head;
+  temp->prev = NULL;
+
+  if(head != NULL)
+  {
+    head->prev = temp;
+  }
+  head = temp;
+}
+
+void print(node* head)
+{
+  node* last;
+  cout<<"In Forward: ";
+  while(head != NULL)
+  {
+    cout<<head->data<<" ";
+    last = head;
+    head = head->next;
+  }
+  cout<<"In Backward: ";
+  while(last != NULL)
+  {
+    cout<<last->data<<" ";
+    last = last->prev;
+  }
 }
 
 int main()
 {
   node* head = NULL;
+  int n;
+  cin>> n;
+  for(int i=0;i<n;i++)
+  {
+    int x;
+    cin>>x;
+    insert(head, x);
+  }
+  print(head);
   return 0;
 }
