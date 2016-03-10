24136a8e7df3bc0d5144917c376dc0d13c9670f4
Rishab Goyal <rishab954@gmail.com>
2016-03-11 00:43:47 +0530

    Insertion

---
+++ Data-Structures/LinkedList/Dobly-LinkedList.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+  int data;
+  node* next;
+  node* prev;
+};
+node* insert(node* head, int data)
+{
+  node* temp = new node();
+  temp->next = NULL;
+}
+
+int main()
+{
+  node* head = NULL;
+  return 0;
+}
